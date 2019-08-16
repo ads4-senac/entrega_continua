8f8f4814821310075af4dd4ed47594451a3072ad
Alex Rabelo Ferreira <arf92@live.com>
2019-08-16 17:24:13 -0300

    Adicao de teste com lorem ipsum para mostrar diff de texto grande

--- projeto_basico/app/src/test/java/dev/alexferreira/projetobasico/ExampleUnitTest.java
+++ projeto_basico/app/src/test/java/dev/alexferreira/projetobasico/ExampleUnitTest.java
@@ -48,7 +48,7 @@ public class ExampleUnitTest {
 
     @Test
     public void whenTestEqual_theyAreEquals() throws Exception {
-        String testedVar = "este teste é um teste bem escrito ";
-        assertEquals("A variavel de texto não é igual a outra, encontre o erro e corrija dev", "este teste é um teste bem escrito", testedVar);
+        String testedVar = "Lorem Ipsum ismply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop pubshing software like Aldus Paeker including versions of Lorem Ipsum.";
+        assertEquals("", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", testedVar);
     }
 }
