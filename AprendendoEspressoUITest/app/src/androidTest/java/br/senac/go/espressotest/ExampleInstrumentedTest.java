e1ce38c3530df7db9175f11eacbcc56d748d84aa
arf92@live.com <gitArfbi099224>
2019-10-17 21:23:15 -0300

    Adicao de scrool

--- AprendendoEspressoUITest/app/src/androidTest/java/br/senac/go/espressotest/ExampleInstrumentedTest.java
+++ AprendendoEspressoUITest/app/src/androidTest/java/br/senac/go/espressotest/ExampleInstrumentedTest.java
@@ -8,9 +8,13 @@ import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.regex.Matcher;
 
 import static org.junit.Assert.assertEquals;
 
@@ -36,11 +40,21 @@ public class ExampleInstrumentedTest {
 
 	@Test
 	public void whenClickBotao_showToast() {
-		Espresso.onView(ViewMatchers.withId(R.id.bt_exemplo)).perform(ViewActions.click());
+		Espresso.onView(ViewMatchers.withId(R.id.bt_exemplo))
+				.perform(ViewActions.click());
 	}
 
 	@Test
 	public void whenStart_showTextHello() {
-		Espresso.onView(ViewMatchers.withId(R.id.tv_hello)).check(ViewAssertions.matches(ViewMatchers.withText("Hello World!")));
+		Espresso.onView(ViewMatchers.withId(R.id.tv_hello))
+				.check(ViewAssertions.matches(ViewMatchers.withText("Hello World!")));
 	}
+
+	@Test
+	public void whenScroll_showText() {
+		Espresso.onView(ViewMatchers.withId(R.id.tv_escondido))
+				.perform(ViewActions.scrollTo())
+				.check(ViewAssertions.matches(Matchers.allOf(ViewMatchers.withText("Texto escondido"), ViewMatchers.isCompletelyDisplayed())));
+	}
+
 }
