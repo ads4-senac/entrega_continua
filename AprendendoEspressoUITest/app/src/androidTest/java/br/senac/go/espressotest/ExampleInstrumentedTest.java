58f832d7bed1a6856ba7af98beca65533fa40f1d
arf92@live.com <gitArfbi099224>
2019-10-17 21:01:13 -0300

    Correcao de projeto

--- AprendendoEspressoUITest/app/src/androidTest/java/br/senac/go/espressotest/ExampleInstrumentedTest.java
+++ AprendendoEspressoUITest/app/src/androidTest/java/br/senac/go/espressotest/ExampleInstrumentedTest.java
@@ -6,6 +6,7 @@ import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,6 +21,10 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+	@Rule
+	public ActivityTestRule<MainActivity> activityRule =
+			new ActivityTestRule<>(MainActivity.class);
 
 	@Test
 	public void useAppContext() {
